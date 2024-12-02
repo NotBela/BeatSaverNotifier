af1a1ca70e93046de5bb545489b6544a81c00d1a
NotBela <27pvarga@gmail.com>
2024-12-01 22:07:41 -0500

    yeah

---
+++ BeatSaverNotifier/BeatSaver/BeatSaverChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Zenject;
+
+namespace BeatSaverNotifier.BeatSaver
+{
+    public class BeatSaverChecker : IInitializable
+    {
+        public event Action OnBeatSaverCheck;
+        
+        public void Initialize()
+        {
+            // check beatsaver on startup
+        }
+    }
+}
