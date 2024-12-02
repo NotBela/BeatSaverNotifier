26e54a55beddd61d41ed2636da0fc17cfb0527c9
NotBela <27pvarga@gmail.com>
2024-12-01 19:31:56 -0500

    bsml

--- BeatSaverNotifier/UI/BSML/BeatSaverNotifierViewController.cs
+++ BeatSaverNotifier/UI/BSML/BeatSaverNotifierViewController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel;
+using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.Settings;
 using BeatSaberMarkupLanguage.ViewControllers;
 using BeatSaverNotifier.Configuration;
@@ -7,6 +8,7 @@ using Zenject;
 
 namespace BeatSaverNotifier.UI
 {
+    [ViewDefinition("BeatSaverNotifier.UI.BSML.BeatSaverNotifierView.bsml")]
     internal class BeatSaverNotifierViewController : BSMLAutomaticViewController
     {
 
