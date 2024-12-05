6d01a2cda16da8e08309df69b0b3698d4bbde112
Bela <27pvarga@gmail.com>
2024-12-05 13:50:09 -0500

    cleanup

--- BeatSaverNotifier/BeatSaver/BeatSaverChecker.cs
+++ BeatSaverNotifier/BeatSaver/BeatSaverChecker.cs
@@ -1,33 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using BeatSaverNotifier.Configuration;
-using Zenject;
 using BeatSaverSharp;
 using BeatSaverSharp.Models;
 using BeatSaverSharp.Models.Pages;
+using IPA.Loader;
 using SiraUtil.Logging;
-using UnityEngine;
+using Zenject;
 
 namespace BeatSaverNotifier.BeatSaver
 {
     public class BeatSaverChecker : IInitializable
     {
         private readonly BeatSaverSharp.BeatSaver _beatSaver = new BeatSaverSharp.BeatSaver(
-            new BeatSaverOptions("BeatSaverNotifier", 
-                IPA.Loader.PluginManager.GetPluginFromId("BeatSaverNotifier").HVersion.ToString()));
-        
-        public event Action<List<Beatmap>> OnBeatSaverCheck;
-        
+            new BeatSaverOptions("BeatSaverNotifier",
+                PluginManager.GetPluginFromId("BeatSaverNotifier").HVersion.ToString()));
+
         private readonly SiraLog _logger;
 
         public BeatSaverChecker(SiraLog logger)
         {
             _logger = logger;
         }
-        
+
         public async void Initialize()
         {
             // check beatsaver on startup
@@ -41,28 +38,24 @@ namespace BeatSaverNotifier.BeatSaver
             }
         }
 
+        public event Action<List<Beatmap>> OnBeatSaverCheck;
+
         private async Task CheckBeatSaverAsync()
         {
-            List<Beatmap> maps = new List<Beatmap>();
-            
+            var maps = new List<Beatmap>();
+
             var listOfMappers = PluginConfig.Instance.followedUsersWithFollowDate;
 
-            if (listOfMappers != null)
-            {
-                foreach (var mapper in listOfMappers)
-                {
-                    var allPages = await getAllRequiredPagesFromUser(mapper);
 
-                    foreach (var page in allPages)
-                    {
-                        foreach (var map in page.Beatmaps)
-                        {
-                            if (map.Uploaded > mapper.Item2) maps.Add(map);
-                        }
-                    }
-                }
+            foreach (var mapper in listOfMappers)
+            {
+                var allPages = await getAllRequiredPagesFromUser(mapper);
+
+                foreach (var page in allPages)
+                    foreach (var map in page.Beatmaps)
+                        if (map.Uploaded > mapper.Item2) maps.Add(map);
             }
-            
+
             OnBeatSaverCheck?.Invoke(maps);
         }
 
@@ -79,7 +72,7 @@ namespace BeatSaverNotifier.BeatSaver
                 currentPageIndex++;
                 currentBeatmapPage = await mapper.Item1.Beatmaps(currentPageIndex);
             }
-            
+
             return returnList;
         }
     }
