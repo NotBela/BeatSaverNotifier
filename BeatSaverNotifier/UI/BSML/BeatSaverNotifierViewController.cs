015638903b873e4053103e995666c56b89d8df93
NotBela <27pvarga@gmail.com>
2024-12-13 17:39:46 -0500

    did work on bsml

--- BeatSaverNotifier/UI/BSML/BeatSaverNotifierViewController.cs
+++ BeatSaverNotifier/UI/BSML/BeatSaverNotifierViewController.cs
@@ -12,9 +12,12 @@ using BeatSaverNotifier.BeatSaver;
 using BeatSaverNotifier.BeatSaver.Auth;
 using BeatSaverNotifier.Configuration;
 using BeatSaverSharp.Models;
+using HMUI;
 using ModestTree;
 using SiraUtil.Logging;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace BeatSaverNotifier.UI
@@ -27,6 +30,8 @@ namespace BeatSaverNotifier.UI
         private SiraLog _logger;
         private OAuthApi _oAuthApi;
         
+        private List<Beatmap> _beatmapsInList;
+        
         [UIComponent("mapList")]
         private readonly CustomListTableData customListTableData = null;
         
@@ -37,7 +42,13 @@ namespace BeatSaverNotifier.UI
             this._beatSaverChecker = beatSaverChecker;
             this._oAuthApi = oAuthApi;
         }
-
+        
+        [UIComponent("rightPanelContainer")] private readonly HorizontalLayoutGroup _rightPanelContainer = null;
+        
+        [UIComponent("mapNameText")] private readonly TextMeshProUGUI mapNameText = null;
+        [UIComponent("mapperNameText")] private readonly TextMeshProUGUI mapperNameText = null;
+        [UIComponent("coverArtImage")] private readonly Image coverArtImage = null;
+        
         [UIAction("testButtonOnClick")]
         private async void testButtonOnClick()
         {
@@ -51,11 +62,37 @@ namespace BeatSaverNotifier.UI
                 _logger.Error(e);
             }
         }
+
+        [UIAction("onCellSelect")]
+        private async void onCellSelected(TableView tableView, int index)
+        {
+            try
+            {
+                var selectedBeatMap = _beatmapsInList[index];
+
+                _rightPanelContainer.gameObject.SetActive(true);
+
+                mapNameText.text = selectedBeatMap.Name;
+                mapperNameText.text = selectedBeatMap.Metadata.LevelAuthorName;
+
+                var downloadedImage = await selectedBeatMap.LatestVersion.DownloadCoverImage();
+                var tex = new Texture2D(1, 1);
+                tex.LoadRawTextureData(downloadedImage);
+                tex.Apply();
+                coverArtImage.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+            }
+        }
         
         private async void OnBeatSaverCheck(List<Beatmap> mapList)
         {
             try
             {
+                this._beatmapsInList = mapList;
+                
                 var mapTableData = new List<CustomListTableData.CustomCellInfo>();
                 foreach (var map in mapList)
                 {
